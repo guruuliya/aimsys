651b50d9821d2d6ce090bcc7f40a25edbcea5870
guruuliya <guru.uliya@gmail.com>
2019-01-21 12:39:53 +0530

    Login Page

---
+++ src/component/Splash.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Splash extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Welcome</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'rgb(32, 53, 70)',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: 'white'
+    }
+});
